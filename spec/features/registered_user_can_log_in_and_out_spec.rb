cff608d4e7246c44a4937105cd12487e393391bf
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-06-05 10:52:19 -0600

    add functionality for registered user to log in and out removed stubbing line for current_user method which was causing test to fail

--- spec/features/registered_user_can_log_in_and_out_spec.rb
+++ spec/features/registered_user_can_log_in_and_out_spec.rb
@@ -23,13 +23,16 @@ RSpec.feature "registered user can log in and out" do
   context "user is logged in" do
     scenario "they log out" do
       user = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
+      
+      visit login_path
 
-      visit user_path(user)
+      fill_in "Username", with: user.username
+      fill_in "Password", with: user.password
+      click_button "Log In"
+
       expect(page).to have_content("Log Out")
 
       click_link "Log Out"
-save_and_open_page
       expect(page).to have_content("Log In")
       expect(page).not_to have_content("Log Out")
     end
