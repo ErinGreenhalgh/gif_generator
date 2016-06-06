987b351ef6ad1d631d606c0ede53b096dfa3b758
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-06-06 09:12:49 -0600

    tweak create_gif class

--- app/models/category/create_gif.rb
+++ app/models/category/create_gif.rb
@@ -1,23 +1,21 @@
-# class Category
 #call this in the controller of whatever action is creating gifs
-  class CreateGif
-    attr_reader :category_name
+class CreateGif
+  attr_reader :category_name
 
-    def initialize(category_name)
-      @category_name = category_name
-      create_association
-    end
-
-    def create_gif
-      gif = Gif.new
-      image = gif.get_image_path(category_name)
-      gif.image_path = image
-      gif
-    end
-
-    def create_association
-      Category.last.gif = create_gif(category_name)
-      #category.find by name
-    end
+  def initialize(category_name)
+    @category_name = category_name
+    create_association
   end
-# end
+
+  def create_gif
+    gif = Gif.new
+    image = gif.get_image_path(category_name)
+    gif.image_path = image
+    gif
+  end
+
+  def create_association
+    Category.last.gif = create_gif(category_name)
+    #category.find by name
+  end
+end
